798a362e7835e0121212f001434c7e1dfab03999
Brahim Akarouch <brahim.akarouch@gmail.com>
2023-09-15 03:08:22 +0100

    chore(docker-compose.yml): add support for loading environment variables from .env file for web and celery services fix(main.py): import missing dependencies (bson.ObjectId and shared_resources.youtube_videos_collection) refactor(main.py): improve code formatting and readability by removing unnecessary blank lines and extra whitespace feat(main.py): add logging statement when a new video is inserted into youtube videos collection

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from bson import ObjectId
+from shared_resources import youtube_videos_collection
 import os
 from flask import Flask, request, jsonify
 import json
@@ -6,8 +8,6 @@ import logging
 import os
 from threading import Thread
 logging.basicConfig(level=logging.INFO)
-from shared_resources import youtube_videos_collection
-from bson import ObjectId
 
 
 app = Flask(__name__)
@@ -17,19 +17,16 @@ app.config.update(
 )
 
 
-
-
-
 @app.route('/process_video', methods=['POST'])
 def process_video():
     data = request.get_json()
     video_url = data.get('video_url')
     public_id = data.get('public_id', 'videos')
-    preferred_resolution = data.get('preferred_resolution','360p')
-
+    preferred_resolution = data.get('preferred_resolution', '360p')
 
     if video_url:
-        task = async_download_and_upload_video.apply_async(args=[video_url, public_id,preferred_resolution])
+        task = async_download_and_upload_video.apply_async(
+            args=[video_url, public_id, preferred_resolution])
         response = {
             'status': 'Video processing started',
             'task_id': task.id,
@@ -44,7 +41,12 @@ def process_video():
 def watch_collection():
     with youtube_videos_collection.watch() as stream:
         for change in stream:
+
             if change['operationType'] == 'insert':
+
+                logging.info(
+                    "New video insterted to youtube videos collection ⭐️")
+
                 new_document = change['fullDocument']
 
                 video_url = new_document['video_url']
@@ -56,8 +58,9 @@ def watch_collection():
                 publisher_topic = str(new_document['publisher_topic'])
 
                 public_id = new_document.get('public_id', 'videos')
-                preferred_resolution = new_document.get('preferred_resolution', '360p')
-                
+                preferred_resolution = new_document.get(
+                    'preferred_resolution', '360p')
+
                 async_download_and_upload_video.apply_async(args=[
                     video_language,
                     video_country,
@@ -68,7 +71,7 @@ def watch_collection():
                     video_url,
                     public_id,
                     preferred_resolution
-                    ])
+                ])
 
 
 if __name__ == "__main__":
